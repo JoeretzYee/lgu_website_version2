c0d8199652e047f9b365327c9beeab10abee70c1
Joeretz <yee.it111@gmail.com>
2026-03-31 14:56:43 +0800

    latest commit 2:56pm

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [youwareVitePlugin(), react()],
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
